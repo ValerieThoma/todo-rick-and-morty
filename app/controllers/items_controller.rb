d22f98d7a5d722ad11313a9a72342cda5e44469a
Valerie Jane <valeriejanethoma@gmail.com>
2019-04-23 20:52:56 -0400

    show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
   end
+
+  def show 
+    
+  end
+
 
   def new 
     @item = Item.new
@@ -21,6 +27,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 
 end
